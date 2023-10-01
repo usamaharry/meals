99c33e65121104e6e678e540ee935b9b97ffa702
Usama Javed <usama.javaid1919@gmail.com>
2023-10-01 18:01:27 +0500

    added meal detail screen

---
+++ lib/screens/meal_detail.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+//local
+import 'package:meals/models/meal.dart';
+
+class MealDetailScreen extends StatelessWidget {
+  final Meal meal;
+  const MealDetailScreen({
+    super.key,
+    required this.meal,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          meal.title,
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.star,
+            ),
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.network(
+              meal.imageUrl,
+              height: 300,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(height: 16),
+            Text(
+              'Ingredients',
+              style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+            ),
+            const SizedBox(height: 8),
+            ...meal.ingredients
+                .map((i) => Text(
+                      i,
+                      style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                            color: Theme.of(context).colorScheme.onBackground,
+                          ),
+                    ))
+                .toList(),
+            const SizedBox(height: 16),
+            Text(
+              'Steps',
+              style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+            ),
+            const SizedBox(height: 8),
+            ...meal.steps
+                .map((i) => Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 10, horizontal: 16),
+                      child: Text(
+                        i,
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                              color: Theme.of(context).colorScheme.onBackground,
+                            ),
+                      ),
+                    ))
+                .toList()
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/widgets/meal_list_item.dart
+++ lib/widgets/meal_list_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meals/screens/meal_detail.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 //local
@@ -24,72 +25,81 @@ class MealListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(
-          10,
-        ),
-      ),
-      clipBehavior: Clip.hardEdge,
-      margin: const EdgeInsets.all(5),
-      child: Stack(
-        children: [
-          FadeInImage(
-            placeholder: MemoryImage(kTransparentImage),
-            image: NetworkImage(
-              meal.imageUrl,
-            ),
-            height: 200,
-            width: double.infinity,
-            fit: BoxFit.cover,
+    return InkWell(
+      onTap: () {
+        Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) => MealDetailScreen(
+            meal: meal,
           ),
-          Positioned(
-            left: 0,
-            right: 0,
-            bottom: 0,
-            child: Container(
-              padding: const EdgeInsets.symmetric(
-                vertical: 10,
-                horizontal: 16,
+        ));
+      },
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(
+            10,
+          ),
+        ),
+        clipBehavior: Clip.hardEdge,
+        margin: const EdgeInsets.all(5),
+        child: Stack(
+          children: [
+            FadeInImage(
+              placeholder: MemoryImage(kTransparentImage),
+              image: NetworkImage(
+                meal.imageUrl,
               ),
-              color: Colors.black54,
-              child: Column(
-                children: [
-                  Text(
-                    meal.title,
-                    textAlign: TextAlign.center,
-                    softWrap: true,
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                    style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                          color: Theme.of(context).colorScheme.onBackground,
-                        ),
-                  ),
-                  const SizedBox(height: 5),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      ListItemTrait(
-                        label: '${meal.duration} min',
-                        icon: Icons.schedule,
-                      ),
-                      const SizedBox(width: 10),
-                      ListItemTrait(
-                        label: complexityText,
-                        icon: Icons.work,
-                      ),
-                      const SizedBox(width: 10),
-                      ListItemTrait(
-                        label: affordabilityText,
-                        icon: Icons.attach_money,
-                      )
-                    ],
-                  )
-                ],
-              ),
+              height: 200,
+              width: double.infinity,
+              fit: BoxFit.cover,
             ),
-          )
-        ],
+            Positioned(
+              left: 0,
+              right: 0,
+              bottom: 0,
+              child: Container(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 16,
+                ),
+                color: Colors.black54,
+                child: Column(
+                  children: [
+                    Text(
+                      meal.title,
+                      textAlign: TextAlign.center,
+                      softWrap: true,
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                            color: Theme.of(context).colorScheme.onBackground,
+                          ),
+                    ),
+                    const SizedBox(height: 5),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ListItemTrait(
+                          label: '${meal.duration} min',
+                          icon: Icons.schedule,
+                        ),
+                        const SizedBox(width: 10),
+                        ListItemTrait(
+                          label: complexityText,
+                          icon: Icons.work,
+                        ),
+                        const SizedBox(width: 10),
+                        ListItemTrait(
+                          label: affordabilityText,
+                          icon: Icons.attach_money,
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
